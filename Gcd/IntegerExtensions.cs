f5f9ee9f5863a5df892d3d36e1d475034a7f3bc7
a91n0d <a91n0d@gmail.com>
2022-01-12 09:21:10 +0300

    Add project files.

---
+++ Gcd/IntegerExtensions.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Gcd
+{
+    /// <summary>
+    /// Provide methods with integers.
+    /// </summary>
+    public static class IntegerExtensions
+    {
+        /// <summary>
+        /// Calculates GCD of two integers from [-int.MaxValue;int.MaxValue] by the Euclidean algorithm.
+        /// </summary>
+        /// <param name="a">First integer.</param>
+        /// <param name="b">Second integer.</param>
+        /// <returns>The GCD value.</returns>
+        /// <exception cref="ArgumentException">Thrown when all numbers are 0 at the same time.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one or two numbers are int.MinValue.</exception>
+        public static int GetGcdByEuclidean(int a, int b)
+        {
+            if (a == int.MinValue)
+            {
+                throw new ArgumentOutOfRangeException(nameof(a), "a number are int.MinValue.");
+            }
+
+            if (b == int.MinValue)
+            {
+                throw new ArgumentOutOfRangeException(nameof(b), "b number are int.MinValue.");
+            }
+
+            if (a == 0 && b == 0)
+            {
+                throw new ArgumentException("All numbers cannot be 0 at the same time.");
+            }
+
+            while (b != 0)
+            {
+                a %= b;
+                (a, b) = (b, a);
+            }
+
+            return Math.Abs(a);
+        }
+
+        /// <summary>
+        /// Calculates GCD of three integers from [-int.MaxValue;int.MaxValue] by the Euclidean algorithm.
+        /// </summary>
+        /// <param name="a">First integer.</param>
+        /// <param name="b">Second integer.</param>
+        /// <param name="c">Third integer.</param>
+        /// <returns>The GCD value.</returns>
+        /// <exception cref="ArgumentException">Thrown when all numbers are 0 at the same time.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one or more numbers are int.MinValue.</exception>
+        public static int GetGcdByEuclidean(int a, int b, int c)
+        {
+            return GetGcdByEuclidean(a, b, new int[] { c });
+        }
+
+        /// <summary>
+        /// Calculates the GCD of integers from [-int.MaxValue;int.MaxValue] by the Euclidean algorithm.
+        /// </summary>
+        /// <param name="a">First integer.</param>
+        /// <param name="b">Second integer.</param>
+        /// <param name="other">Other integers.</param>
+        /// <returns>The GCD value.</returns>
+        /// <exception cref="ArgumentException">Thrown when all numbers are 0 at the same time.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one or more numbers are int.MinValue.</exception>
+        public static int GetGcdByEuclidean(int a, int b, params int[] other)
+        {
+            Array.Resize(ref other, other.Length + 2);
+            other[^1] = b;
+            other[^2] = a;
+            int gcdByEuclidean = 0;
+            for (int i = 0; i < other.Length; i++)
+            {
+                if (other[i] != 0)
+                {
+                    gcdByEuclidean = GetGcdByEuclidean(gcdByEuclidean, other[i]);
+                }
+            }
+
+            if (gcdByEuclidean == 0)
+            {
+                throw new ArgumentException("All numbers cannot be 0 at the same time.");
+            }
+
+            return gcdByEuclidean;
+        }
+
+        /// <summary>
+        /// Calculates GCD of two integers from [-int.MaxValue;int.MaxValue] by the Euclidean algorithm with elapsed time.
+        /// </summary>
+        /// <param name="elapsedTicks">Method execution time in ticks.</param>
+        /// <param name="a">First integer.</param>
+        /// <param name="b">Second integer.</param>
+        /// <returns>The GCD value.</returns>
+        /// <exception cref="ArgumentException">Thrown when all numbers are 0 at the same time.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one or two numbers are int.MinValue.</exception>
+        public static int GetGcdByEuclidean(out long elapsedTicks, int a, int b)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int gcdByEuclidean = GetGcdByEuclidean(a, b);
+            stopwatch.Stop();
+            elapsedTicks = stopwatch.ElapsedTicks;
+            return gcdByEuclidean;
+        }
+
+        /// <summary>
+        /// Calculates GCD of three integers from [-int.MaxValue;int.MaxValue] by the Euclidean algorithm with elapsed time.
+        /// </summary>
+        /// <param name="elapsedTicks">Method execution time in ticks.</param>
+        /// <param name="a">First integer.</param>
+        /// <param name="b">Second integer.</param>
+        /// <param name="c">Third integer.</param>
+        /// <returns>The GCD value.</returns>
+        /// <exception cref="ArgumentException">Thrown when all numbers are 0 at the same time.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one or more numbers are int.MinValue.</exception>
+        public static int GetGcdByEuclidean(out long elapsedTicks, int a, int b, int c)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int gcdByEuclidean = GetGcdByEuclidean(a, b, c);
+            stopwatch.Stop();
+            elapsedTicks = stopwatch.ElapsedTicks;
+            return gcdByEuclidean;
+        }
+
+        /// <summary>
+        /// Calculates the GCD of integers from [-int.MaxValue;int.MaxValue] by the Euclidean algorithm with elapsed time.
+        /// </summary>
+        /// <param name="elapsedTicks">Method execution time in Ticks.</param>
+        /// <param name="a">First integer.</param>
+        /// <param name="b">Second integer.</param>
+        /// <param name="other">Other integers.</param>
+        /// <returns>The GCD value.</returns>
+        /// <exception cref="ArgumentException">Thrown when all numbers are 0 at the same time.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one or more numbers are int.MinValue.</exception>
+        public static int GetGcdByEuclidean(out long elapsedTicks, int a, int b, params int[] other)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int gcdByEuclidean = GetGcdByEuclidean(a, b, other);
+            stopwatch.Stop();
+            elapsedTicks = stopwatch.ElapsedTicks;
+            return gcdByEuclidean;
+        }
+
+        /// <summary>
+        /// Calculates GCD of two integers [-int.MaxValue;int.MaxValue] by the Stein algorithm.
+        /// </summary>
+        /// <param name="a">First integer.</param>
+        /// <param name="b">Second integer.</param>
+        /// <returns>The GCD value.</returns>
+        /// <exception cref="ArgumentException">Thrown when all numbers are 0 at the same time.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one or two numbers are int.MinValue.</exception>
+        public static int GetGcdByStein(int a, int b)
+        {
+            if (a == int.MinValue || b == int.MinValue)
+            {
+                throw new ArgumentOutOfRangeException(nameof(a), "One or two numbers are int.MinValue.");
+            }
+
+            if (a == 0 && b == 0)
+            {
+                throw new ArgumentException("All numbers are 0 at the same time");
+            }
+
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+
+            if (a == 0)
+            {
+                return b;
+            }
+
+            if (b == 0)
+            {
+                return a;
+            }
+
+            if (a == b)
+            {
+                return a;
+            }
+
+            bool aIsEven = (a & 1) == 0;
+            bool bIsEven = (b & 1) == 0;
+            if (aIsEven && bIsEven)
+            {
+                return GetGcdByStein(a >> 1, b >> 1) << 1;
+            }
+            else if (aIsEven && !bIsEven)
+            {
+                return GetGcdByStein(a >> 1, b);
+            }
+            else if (bIsEven)
+            {
+                return GetGcdByStein(a, b >> 1);
+            }
+            else if (a > b)
+            {
+                return GetGcdByStein((a - b) >> 1, b);
+            }
+            else
+            {
+                return GetGcdByStein(a, (b - a) >> 1);
+            }
+        }
+
+        /// <summary>
+        /// Calculates GCD of three integers [-int.MaxValue;int.MaxValue] by the Stein algorithm.
+        /// </summary>
+        /// <param name="a">First integer.</param>
+        /// <param name="b">Second integer.</param>
+        /// <param name="c">Third integer.</param>
+        /// <returns>The GCD value.</returns>
+        /// <exception cref="ArgumentException">Thrown when all numbers are 0 at the same time.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one or more numbers are int.MinValue.</exception>
+        public static int GetGcdByStein(int a, int b, int c)
+        {
+            return GetGcdByStein(a, b, new int[] { c });
+        }
+
+        /// <summary>
+        /// Calculates the GCD of integers [-int.MaxValue;int.MaxValue] by the Stein algorithm.
+        /// </summary>
+        /// <param name="a">First integer.</param>
+        /// <param name="b">Second integer.</param>
+        /// <param name="other">Other integers.</param>
+        /// <returns>The GCD value.</returns>
+        /// <exception cref="ArgumentException">Thrown when all numbers are 0 at the same time.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one or more numbers are int.MinValue.</exception>
+        public static int GetGcdByStein(int a, int b, params int[] other)
+        {
+            Array.Resize(ref other, other.Length + 2);
+            other[^1] = b;
+            other[^2] = a;
+            int gcdByStein = 0;
+            for (int i = 0; i < other.Length; i++)
+            {
+                if (other[i] != 0)
+                {
+                    gcdByStein = GetGcdByStein(gcdByStein, other[i]);
+                }
+            }
+
+            if (gcdByStein == 0)
+            {
+                throw new ArgumentException("All numbers cannot be 0 at the same time.");
+            }
+
+            return gcdByStein;
+        }
+
+        /// <summary>
+        /// Calculates GCD of two integers from [-int.MaxValue;int.MaxValue] by the Stein algorithm with elapsed time.
+        /// </summary>
+        /// <param name="elapsedTicks">Method execution time in ticks.</param>
+        /// <param name="a">First integer.</param>
+        /// <param name="b">Second integer.</param>
+        /// <returns>The GCD value.</returns>
+        /// <exception cref="ArgumentException">Thrown when all numbers are 0 at the same time.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one or two numbers are int.MinValue.</exception>
+        public static int GetGcdByStein(out long elapsedTicks, int a, int b)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int gcdByStein = GetGcdByStein(a, b);
+            stopwatch.Stop();
+            elapsedTicks = stopwatch.ElapsedTicks;
+            return gcdByStein;
+        }
+
+        /// <summary>
+        /// Calculates GCD of three integers from [-int.MaxValue;int.MaxValue] by the Stein algorithm with elapsed time.
+        /// </summary>
+        /// <param name="elapsedTicks">Method execution time in ticks.</param>
+        /// <param name="a">First integer.</param>
+        /// <param name="b">Second integer.</param>
+        /// <param name="c">Third integer.</param>
+        /// <returns>The GCD value.</returns>
+        /// <exception cref="ArgumentException">Thrown when all numbers are 0 at the same time.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one or more numbers are int.MinValue.</exception>
+        public static int GetGcdByStein(out long elapsedTicks, int a, int b, int c)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int gcdByStein = GetGcdByStein(a, b, c);
+            stopwatch.Stop();
+            elapsedTicks = stopwatch.ElapsedTicks;
+            return gcdByStein;
+        }
+
+        /// <summary>
+        /// Calculates the GCD of integers from [-int.MaxValue;int.MaxValue] by the Stein algorithm with elapsed time.
+        /// </summary>
+        /// <param name="elapsedTicks">Method execution time in Ticks.</param>
+        /// <param name="a">First integer.</param>
+        /// <param name="b">Second integer.</param>
+        /// <param name="other">Other integers.</param>
+        /// <returns>The GCD value.</returns>
+        /// <exception cref="ArgumentException">Thrown when all numbers are 0 at the same time.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one or more numbers are int.MinValue.</exception>
+        public static int GetGcdByStein(out long elapsedTicks, int a, int b, params int[] other)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int gcdByStein = GetGcdByStein(a, b, other);
+            stopwatch.Stop();
+            elapsedTicks = stopwatch.ElapsedTicks;
+            return gcdByStein;
+        }
+    }
+}
